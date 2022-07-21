e6830675555f209c989232698de474d971f6e7b4
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-07-21 19:06:22 +0300

    HW_3.1_Gnilitskiy

--- HW_1_Gnilitskiy_SwiftUI/ContentView.swift
+++ HW_1_Gnilitskiy_SwiftUI/ContentView.swift
@@ -2,20 +2,56 @@
 //  ContentView.swift
 //  HW_1_Gnilitskiy_SwiftUI
 //
-//  Created by Александр on 19.07.2022.
+//  Created by Александр on 21.07.2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var buttonText = "START"
+    @State private var glowRed = 0.3
+    @State private var glowYellow = 0.3
+    @State private var glowGreen = 0.3
+    @State private var indexSwitch = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ColorCircle(
+                colorOne: .red.opacity(glowRed),
+                colorTwo: .yellow.opacity(glowYellow),
+                colorThree: .green.opacity(glowGreen)
+            )
+            
+            Button(action: {
+                buttonText = "NEXT"
+                if indexSwitch == 1 {
+                    glowRed = 1.0
+                    glowYellow = 0.3
+                    glowGreen = 0.3
+                    indexSwitch = 2
+                } else if indexSwitch == 2 {
+                    glowRed = 0.3
+                    glowYellow = 1.0
+                    glowGreen = 0.3
+                    indexSwitch = 3
+                } else if indexSwitch == 3 {
+                    glowRed = 0.3
+                    glowYellow = 0.3
+                    glowGreen = 1.0
+                    indexSwitch = 1
+                }
+            }){
+                Text("\(buttonText)")
+                    .font(.title)
+            }
+            .offset(x: 0, y: 80)
+        }
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
